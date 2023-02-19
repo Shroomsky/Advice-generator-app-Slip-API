8ce795e1ec1e0fcecfdf1b6459188e6a76c2c8f0
Piotr Grzybowski <grzybowski_p@wp.pl>
2023-02-19 17:35:06 +0100

    check if id is same

--- Js/main.js
+++ Js/main.js
@@ -4,24 +4,32 @@ const diceBtn = document.querySelector(".dice");
 const url = "https://api.adviceslip.com/advice";
 
 const getData = () => {
-    let id = 0;
-	let advice = "";
+	let id = 0;
+	let text = "";
 	fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        id = data.slip.id;
-        advice = data.slip.advice;
-        span.innerText = id;
-        p.innerText = advice;
-    });
-    rollAnimation();
+		.then((res) => res.json())
+		.then((data) => {
+			id = data.slip.id;
+			text = data.slip.advice;
+			if (Number(span.innerText) == id) {
+				getData();
+			} else {
+				render(id, text);
+			};
+		});
+	rollAnimation();
 };
 
-
-const rollAnimation = () => {
-    diceBtn.classList.add("roll");
-    setTimeout(() => diceBtn.classList.remove("roll"), "800");
+const render = (id, text) => {
+	span.innerText = id;
+	p.innerText = text;
 };
+
+function rollAnimation() {
+	diceBtn.classList.add("roll");
+	setTimeout(() => diceBtn.classList.remove("roll"), "800");
+}
+
 getData();
 
 diceBtn.addEventListener("click", getData);
